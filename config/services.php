a713f6350f291715a8536d25df577ae5b8ecde9e
Afif Nur Fauzi <alscheift@gmail.com>
2024-03-31 13:39:43 +0700

    Google Secret Key in env and services

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         ],
     ],
 
+    'recaptcha' => [
+        'site_key' => env('RECAPTCHA_SITE_KEY'),
+        'secret_key' => env('RECAPTCHA_SECRET_KEY'),
+        'min_score' => env('RECAPTCHA_MIN_SCORE', .5),
+    ],
+
 ];
